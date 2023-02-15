b1c7ef2f2263f07e34a27e07b72615d0cdabdafd
satyajit-n <namawar.satyajeet@gmail.com>
2023-02-15 13:06:51 +0530

    first DOM commit

---
+++ dompractice.js
@@ -0,0 +1,34 @@
+//Examine Document Object
+
+//console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// // console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links);
+//console.log(document.images);
+
+
+// GETELEMENTBYID
+
+// console.log(document.getElementById('header-title'))
+
+let headertitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+
+// console.log(headertitle)
+
+// headertitle.textContent = 'hello';
+// headertitle.innerText = 'goodbye';
+
+console.log(headertitle.textContent);
+console.log(headertitle.innerText);
+
+// header.style.borderBottom = 'solid 3px #000';
+
