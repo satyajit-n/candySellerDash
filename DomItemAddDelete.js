8edc4f847a0cda0a1a3357bbd825c01b5cb4a12c
satyajit-n <namawar.satyajeet@gmail.com>
2023-02-17 11:59:40 +0530

    Filter COMMIT

--- DomItemAddDelete.js
+++ DomItemAddDelete.js
@@ -1,6 +1,6 @@
 let form = document.getElementById("addForm");
 let itemList = document.getElementById("items");
-var filter = document.getElementById('filter');
+var filter = document.getElementById("filter");
 
 //form submit event
 form.addEventListener("submit", addItem);
@@ -9,7 +9,7 @@ form.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 
 //filter event
-filter.addEventListener('keyup',filterItems);
+filter.addEventListener("keyup", filterItems);
 
 //add item function
 function addItem(e) {
@@ -17,25 +17,33 @@ function addItem(e) {
 
   // getting submitted value
   let newItem = document.getElementById("item").value;
+  let newItemDescription = document.getElementById("description").value;
 
   //create new list element
   let li = document.createElement("li");
+
   //adding class name to new li
   li.className = "list-group-item";
+
   //add text node with input value
   li.append(document.createTextNode(newItem));
+  li.append(document.createTextNode(newItemDescription));
 
   //create delete button element
   let deleteButton = document.createElement("button");
+  let editButton = document.createElement("button");
 
   //add classes to delete button
   deleteButton.className = "btn btn-danger btn-sm float-right delete";
+  editButton.className = "glyphicon glyphicon-edit btn btn-sm btn-outline-info float-right";
 
   //append text node
   deleteButton.appendChild(document.createTextNode("X"));
+  editButton.appendChild(document.createTextNode("Edit"));
 
   //append button to li
   li.appendChild(deleteButton);
+  li.appendChild(editButton);
   //append li to list
   itemList.appendChild(li);
 }
@@ -50,22 +58,23 @@ function removeItem(e) {
   }
 }
 
-
 //filter Function
-function filterItems(e){
-    //convert text to lower text
-    var text = e.target.value.toLowerCase();
+function filterItems(e) {
+  //convert text to lower text
+  var text = e.target.value.toLowerCase();
 
-    //get all li elements
-    var items = itemList.getElementsByTagName('li');
-    //convert to array
-    Array.from(items).forEach(function(item){
-        var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text)!= -1){
-            item.style.display = 'block';
-        }else{
-            item.style.display = 'none';
-        }
-    })
+  //get all li elements
+  var items = itemList.getElementsByTagName("li");
 
+  //convert to array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+    console.log(itemName)
+    if (itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
